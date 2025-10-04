050e7469ec151779cdc36ecc84735544e0ff9e9c
Prani2018 <46190678+Prani2018@users.noreply.github.com>
2025-10-03 23:25:38 -0500

    Update eks-backend.tf

--- eks-cluster/eks-backend.tf
+++ eks-cluster/eks-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "tf-state-files-09262025"
     key            = "eks/cluster-state-${var.aws_region}.tfstate" # Separate state file per region
     region         = "us-east-1" # State bucket location (should be static)
     encrypt        = true
