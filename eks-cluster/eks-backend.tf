d051762a98d3f3484e5213fc62f78cf98ab412ad
jbossadmin <jbossadmin@gmail.com>
2025-10-04 11:37:15 -0500

    changes to jenkins files, eks-backend.tf

--- eks-cluster/eks-backend.tf
+++ eks-cluster/eks-backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-files-09262025"
-    key            = "eks/cluster-state-${var.aws_region}.tfstate" # Separate state file per region
+    key            = "eks/cluster-state-us-east-1.tfstate" # Separate state file per region
     region         = "us-east-1" # State bucket location (should be static)
     encrypt        = true
   }
