63bad70399c459281f381a1effbaedb6ebf68eb0
Prani2018 <46190678+Prani2018@users.noreply.github.com>
2025-10-03 23:29:04 -0500

    Update eks-variables.tf

--- eks-cluster/eks-variables.tf
+++ eks-cluster/eks-variables.tf
@@ -1,3 +1,8 @@
 variable "vpc_cidr_block" {}
 variable "private_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
+variable "aws_region" {
+  description = "The AWS region to deploy the resources into"
+  type        = string
+  default     = "us-east-1" # Set a safe default
+}
