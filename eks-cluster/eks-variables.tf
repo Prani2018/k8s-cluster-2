64bc9f100d2618ace1f49c14b25552c254fa113e
jbossadmin <jbossadmin@gmail.com>
2025-10-04 11:45:33 -0500

    updated variables

--- eks-cluster/eks-variables.tf
+++ eks-cluster/eks-variables.tf
@@ -4,9 +4,11 @@ variable "public_subnet_cidr_blocks" {}
 variable "aws_region" {
   description = "The AWS region to deploy the resources into"
   type        = string
+  value       = "us-east-1"
 }
 
 variable "cluster_name" {
   description = "The unique name for the EKS cluster and related resources"
   type        = string
+  value       = "Cluster-A"
 }
